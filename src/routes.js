72477d2aaa77a5777ba2b32ab1ae6efb4d7989e4
renanvilabela <vilabelarenan@gmail.com>
2023-06-22 19:23:27 -0300

    ajustes nos arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
+const SkateController = require('./controllers/skateController');
+
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path:'variaveis.env'});
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routes');
 const bodyParser = require('body-parser');
 
 const router = require('./routes.js');
@@ -8,6 +9,8 @@ const router = require('./routes.js');
 const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({extended:false}));
+
+server.use('/api', routes);
 
 server.listen(process.env.PORT, ()=>{
     console.log(`servidor rodando em: http://localhost:${process.env.PORT}`);
